55fe6cc35735dfa8d57e4ef6a1f983ac5eaa594c
sonbyungjun <saint2030@naver.com>
2019-03-19 10:26:36 +0900

    53day

---
+++ java-basic/src/main/java/ch29/e/Test03.java
@@ -0,0 +1,15 @@
+package ch29.e;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test03 {
+  public static void main(String[] args) {
+    ApplicationContext iocContainer = 
+        new ClassPathXmlApplicationContext("ch29/e/application-context-03.xml");
+    
+    System.out.println("------------------------------------------");
+    
+    System.out.println(iocContainer.getBean("c1"));
+  }
+}
---
+++ java-basic/src/main/java/ch29/e/Test04.java
@@ -0,0 +1,15 @@
+package ch29.e;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test04 {
+  public static void main(String[] args) {
+    ApplicationContext iocContainer = 
+        new ClassPathXmlApplicationContext("ch29/e/application-context-04.xml");
+    
+    System.out.println("------------------------------------------");
+    
+    System.out.println(iocContainer.getBean("t1"));
+  }
+}
