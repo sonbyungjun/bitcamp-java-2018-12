913e268f866c6fa81e11d4d2ed2905e9e2a45590
sonbyungjun <saint2030@naver.com>
2019-03-21 09:41:29 +0900

    55day am

---
+++ java-basic/src/main/java/ch30/a/Test01.java
@@ -0,0 +1,22 @@
+package ch30.a;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test01 {
+  public static void main(String[] args) {
+    ApplicationContext iocContainer = 
+        new ClassPathXmlApplicationContext("ch30/a/application-context-01.xml");
+    
+    System.out.println("------------------------------------------");
+    
+    String[] names = iocContainer.getBeanDefinitionNames();
+    for (String name : names) {
+      System.out.printf("%s ==> %s\n" , name, iocContainer.getBean(name).getClass().getName());
+    }
+    
+    System.out.println("------------------------------------------");
+    
+    System.out.println(iocContainer.getBean("car"));
+  }
+}
