d81a0c2acd2c14eee75c97e3d4601e99286c6dcb
Byungjun Son <saint2030@naver.com>
2019-05-08 11:26:53 +0900

    2019-5-8

--- java-web-project/src/main/webapp/html/board/index.js
+++ java-web-project/src/main/webapp/html/board/index.js
@@ -1,53 +1,56 @@
 var pageNo = 1,
-pageSize = 3,
-tbody = document.querySelector('tbody'),
-prevPageLi = document.querySelector('#prevPage'),
-nextPageLi = document.querySelector('#nextPage'),
-currSpan = document.querySelector('#currPage > span');
+    pageSize = 3,
+    tbody = $('tbody'),
+    prevPageLi = $('#prevPage'),
+    nextPageLi = $('#nextPage'),
+    currSpan = $('#currPage > span');
 
 //데이터 목록 가져오기
 function loadList(pn) {
+  
   $.getJSON('../../app/json/board/list?pageNo=' + pn + '&pageSize=' + pageSize, function(obj) {
       pageNo = obj.pageNo;
-      tbody.innerHTML = '';
+      tbody.html('');
       for (data of obj.list) {
         var tr = '<tr>'
           + '<th scope="row">' + data.no + '</th>'
-          //+ '<td>' + '<a href="view.html?no=' + data.no + '">' + data.contents + '</td>'
           + '<td>' + '<a class="bit-view-link" href="#" data-no="' + data.no + '">' + data.contents + '</td>'
           + '<td>' + data.createdDate + '</td>'
           + '<td>' + data.viewCount + '</td>'
           + '</tr>';
-        tbody.innerHTML += tr;
+        tbody.html(tbody.html() + tr);
       }
-      currSpan.innerHTML = String(pageNo);
+      
+      currSpan.html(String(pageNo));
+      
       if (pageNo == 1) {
-        prevPageLi.className += ' disabled';
+        prevPageLi.addClass(' disabled');
       } else {
-        prevPageLi.className = prevPageLi.className.replace(' disabled', '');
+        prevPageLi.removeClass(' disabled');
       }
       if (pageNo == obj.totalPage) {
-        nextPageLi.className += ' disabled';
+        nextPageLi.addClass(' disabled');
       } else {
-        nextPageLi.className = nextPageLi.className.replace(' disabled', '');
+        nextPageLi.removeClass(' disabled');
       }
+      
       document.body.dispatchEvent(new Event('loaded-list'));
   }); // Bitcamp.getJSON()
 } //loadList() 
 
-document.querySelector('#prevPage > a').onclick = (e) => {
+$('#prevPage > a').click((e) => {
   e.preventDefault();
   loadList(pageNo - 1);
-};
+});
 
-document.querySelector('#nextPage > a').onclick = (e) => {
+$('#nextPage > a').click((e) => {
   e.preventDefault();
   loadList(pageNo + 1);
-};
+});
 
 loadList(1);
 
-document.body.addEventListener('loaded-list', () => {
+$(document.body).bind('loaded-list', () => {
   // 제목을 클릭 했을 때 view.html로 전환시키기
   var alist = document.querySelectorAll('.bit-view-link');
   for (a of alist) {
--- java-web-project/src/main/webapp/js/bitcamp.js
+++ java-web-project/src/main/webapp/js/bitcamp.js
@@ -1,4 +1,64 @@
-var Bitcamp = {};
+var Bitcamp = function(arg1) {
+  var arr = [];
+
+  if (arg1 instanceof HTMLElement) {
+    arr.push(arg1);
+  } else {
+    var el = document.querySelectorAll(arg1);
+
+    for (var e of el) {
+      arr.push(e);
+    } 
+  }
+
+  arr.html = function(value) {
+    if (arguments.length > 0) {
+      for (var e of this) {
+        e.innerHTML = value;
+      }
+    } else {
+      return this[0].innerHTML;
+    }
+  };
+
+  arr.addClass = function(value) {
+    for (var e of this) {
+      var cn = '';
+      var names = e.className.split(' ');
+      for (var name of names) {
+        cn = cn + ' ' + name;
+      }
+      e.className = cn;
+    }
+  };
+
+  arr.removeClass = function(value) {
+    for (var e of this) {
+      var cn = '';
+      var names = e.className.split(' ');
+      for (var name of names) {
+        if (name == value)
+          continue;
+        cn = cn + ' ' + name;
+      }
+      e.className = cn;
+    }
+  };
+
+  arr.click = function(cb) {
+    for (var e of this) {
+      e.addEventListener('click', cb);
+    }
+  };
+  
+  arr.bind = function(eventType, handler) {
+    for (var e of this) {
+      e.addEventListener(eventType, handler);
+    }
+  };
+
+  return arr;
+};
 
 Bitcamp.ajax = function(url, settings) {
   if (settings == undefined || settings == null) {
@@ -10,7 +70,7 @@ Bitcamp.ajax = function(url, settings) {
   if (!settings.dataType) {
     settings.dataType = 'text';
   }
-  
+
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState != 4)
